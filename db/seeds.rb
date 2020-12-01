027c48da692ee8361d0952513821aeef87c48e0b
Chebassier Baptiste <baptiste.chebassier28@gmail.com>
2020-12-01 22:54:41 +0100

    some typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
     Galerie.destroy_all
-  		results = Cloudinary::Api.resources_by_tag("Viivan", max_results: 100)
+  		results = Cloudinary::Api.resources_by_tag("Viivan", max_results: 110)
   		jon = results.first[1]
   		jon.each do |image|
 			beach = Galerie.new(nom: image['public_id'], url: image['url'])
